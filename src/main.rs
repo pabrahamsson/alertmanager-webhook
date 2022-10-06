18a12bac6cb653361560e3e8e1b026f7b8a68e5d
Petter Abrahamsson <pabraham@redhat.com>
2022-10-05 22:14:43 -0400

    Refactor Discord message creation
    - Add  endpoint\n- Prepare for v0.2.1

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type Result<T> = std::result::Result<T, GenericError>;
 
 static NOTFOUND: &[u8] = b"Not Found";
+static OK: &[u8] = b"ok";
 static COLOR_GREEN: u32 = 3066993;
 static COLOR_RED: u32 = 10038562;
 
@@ -36,7 +37,6 @@ struct AMAlerts {
 #[allow(dead_code)]
 struct AMAlert {
     annotations: HashMap<String, String>,
-    //annotations: Value,
     ends_at: String,
     fingerprint: String,
     #[serde(rename = "generatorURL")]
@@ -80,46 +80,49 @@ async fn discord_alert(client: &Client<HttpsConnector<HttpConnector>>, message: 
     Ok(Response::new(res.into_body()))
 }
 
+async fn get_description(annotations: &HashMap<String,String>) -> String {
+    if annotations.contains_key(&String::from("description")) {
+        annotations["description"].clone()
+    } else if annotations.contains_key(&String::from("message")) {
+        annotations["message"].clone()
+    } else {
+        "No 'description' or 'message' annotation found...".to_string()
+    }
+}
+
+async fn create_embed(alert: &AMAlert) -> DiscordEmbed {
+    let color = if alert.status == String::from("firing") {
+        COLOR_RED
+    } else {
+        COLOR_GREEN
+    };
+    let description = get_description(&alert.annotations).await;
+    let embed = DiscordEmbed {
+        title: format!("{} - {} ({})",
+            alert.labels["alertname"].as_str().unwrap(),
+            alert.status.as_str().to_capitalized(),
+            alert.labels["severity"].as_str().unwrap().to_uppercase()
+        ),
+        description: description.to_string(),
+        color: color,
+    };
+    embed
+}
+
 async fn alerts_post_response(req: Request<Body>, client: &Client<HttpsConnector<HttpConnector>>) -> Result<Response<Body>> {
     let whole_body = hyper::body::aggregate(req).await?;
     let alerts: AMAlerts = serde_json::from_reader(whole_body.reader())?;
     let mut embeds = Vec::new();
     for alert in &alerts.alerts {
-        let color = if alert.status == String::from("firing") {
-            COLOR_RED
-        }  else {
-            COLOR_GREEN
-        };
-        let description = if alert.annotations.contains_key(&String::from("description")) {
-            &alert.annotations["description"]
-        } else if alert.annotations.contains_key(&String::from("message")) {
-            &alert.annotations["message"]
-        } else {
-            "No 'description' or 'message' annotation found..."
-        };
-        let embed = DiscordEmbed {
-            title: format!("{} - {} ({})",
-                       alert.labels["alertname"].as_str().unwrap(),
-                       alert.status.as_str().to_capitalized(),
-                       alert.labels["severity"].as_str().unwrap().to_uppercase()
-            ),
-            //description: alert.annotations.to_string(),
-            description: description.to_string(),
-            color: color,
-        };
+        let embed = create_embed(&alert).await;
         embeds.push(embed);
     };
-    let discord_message = DiscordMessage {
-        /*
-        content: format!("[{}: {}] {}",
-                         alerts.status.as_str().to_capitalized(),
-                         alerts.alerts.len(),
-                         alerts.common_labels["alertname"].as_str().unwrap(),
-        ),
-        */
-        embeds: embeds,
-    };
-    discord_alert(client, discord_message).await?;
+    if embeds.len() > 0 {
+        let discord_message = DiscordMessage {
+            embeds: embeds,
+        };
+        discord_alert(client, discord_message).await?;
+    }
     let response = Response::builder()
         .status(StatusCode::OK)
         .header(header::CONTENT_TYPE, "application/json")
@@ -133,6 +136,12 @@ async fn response_handler(
 ) -> Result<Response<Body>> {
     match (req.method(), req.uri().path()) {
         (&Method::POST, "/alerts") => alerts_post_response(req, &client).await,
+        (&Method::GET, "/healthz") => {
+            Ok(Response::builder()
+               .status(StatusCode::OK)
+               .body(OK.into())
+               .unwrap())
+        },
         _ => {
             Ok(Response::builder()
                .status(StatusCode::NOT_FOUND)
